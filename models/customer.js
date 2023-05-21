f60970d14a54cca02de8a02686b26b39cc57aff2
VinceVervaele <vince.vervaele@student.vives.be>
2023-05-21 10:41:22 +0200

    index aangemaakt, customer model met joi validatie, nodige modules gedownload

---
+++ models/customer.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  address: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 125
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+}));
+
+function validateCustomer(customer) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    address: Joi.min(5).max(125).required(),
+    phone: Joi.string().min(5).max(50).required()
+  });
+
+  return schema.validate(customer);
+}
+
+exports.Customer = Customer; 
+exports.validate = validateCustomer;
